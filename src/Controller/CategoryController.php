35fed101fc30c8c07dab563d418b41744c385200
Florian <florian.frazao@gmail.com>
2021-07-06 10:27:20 +0200

    step1

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,27 +7,31 @@ class CategoryController extends AbstractController
 {
     private $categories = [
         1 => [
-            "title" => "Politique",
-            "content" => "Tous les articles liés à Jean Lassalle",
             "id" => 1,
+            "title" => "Politique",
+            "desc" => "Tous les articles liés à Jean Lassalle",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur condimentum nunc, at ullamcorper odio pretium a. Fusce vitae interdum lectus, eu lacinia neque. Quisque ut laoreet magna, quis dictum quam. Curabitur lorem lectus, fermentum ut ante at, tempus fringilla leo. Sed nec facilisis velit, ultricies interdum nibh. Curabitur neque arcu, pulvinar vitae libero sed, congue dignissim erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
             "published" => true,
         ],
         2 => [
-            "title" => "Economie",
-            "content" => "Les meilleurs tuyaux pour avoir DU FRIC",
             "id" => 2,
+            "title" => "Economie",
+            "desc" => "Les meilleurs tuyaux pour avoir DU FRIC",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur condimentum nunc, at ullamcorper odio pretium a. Fusce vitae interdum lectus, eu lacinia neque. Quisque ut laoreet magna, quis dictum quam. Curabitur lorem lectus, fermentum ut ante at, tempus fringilla leo. Sed nec facilisis velit, ultricies interdum nibh. Curabitur neque arcu, pulvinar vitae libero sed, congue dignissim erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
             "published" => true
         ],
         3 => [
-            "title" => "Securité",
-            "content" => "Attention les étrangers sont très méchants",
             "id" => 3,
+            "title" => "Securité",
+            "desc" => "Attention les étrangers sont très méchants",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur condimentum nunc, at ullamcorper odio pretium a. Fusce vitae interdum lectus, eu lacinia neque. Quisque ut laoreet magna, quis dictum quam. Curabitur lorem lectus, fermentum ut ante at, tempus fringilla leo. Sed nec facilisis velit, ultricies interdum nibh. Curabitur neque arcu, pulvinar vitae libero sed, congue dignissim erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
             "published" => true
         ],
         4 => [
-            "title" => "Ecologie",
-            "content" => "Hummer <3",
             "id" => 4,
+            "title" => "Ecologie",
+            "desc" => "Hummer <3",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur condimentum nunc, at ullamcorper odio pretium a. Fusce vitae interdum lectus, eu lacinia neque. Quisque ut laoreet magna, quis dictum quam. Curabitur lorem lectus, fermentum ut ante at, tempus fringilla leo. Sed nec facilisis velit, ultricies interdum nibh. Curabitur neque arcu, pulvinar vitae libero sed, congue dignissim erat. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
             "published" => true
         ]
     ];
