b65ce1e45e1c9f864d0cceb3197212c4e0f34647
Florian <florian.frazao@gmail.com>
2021-07-15 16:06:01 +0200

    changed css

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -2,6 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -19,21 +22,38 @@ class ArticleType extends AbstractType
             ->add('title', TextType::class, [
                 'label' => 'Titre' // change le nom du sslabel
             ])
+
             ->add('description', TextType::class, [
                 'label' => 'Description'
             ])
+
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
+
+            ->add('tag', EntityType::class, [
+                'label' => 'Tag',
+                'class' => Tag::class,
+                'choice_label' => 'title'
+            ])
+
             ->add('content', TextareaType::class, [
                 'label' => 'Contenu'
             ])
+
             ->add('createdAt', DateType::class, [
                 'widget' => 'single_text', // convertit le champ date en champ date HTML5
                 'label' => "Date de création",
                 'data' => new \DateTime('NOW') // affiche la date du jour
             ])
+
             ->add('isPublished', CheckboxType::class, [
                 'label' => 'Publié ?',
                 'data' => true // autochecked
             ])
+
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer'
             ])
